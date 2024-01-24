994f88e5de37e9d0739adad5a4ea98822c0f499e
Asseel Abdalla <asseel.abdalla@gmail.com>
2024-01-23 21:10:31 -0500

    Scaffold Courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :courses
   root 'home#index'
-  resources :students
+  resources :students, except: :index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
